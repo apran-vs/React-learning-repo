72856f9bcd946dcce939108ae6ef468db483f20a
Arpan Shingala <202003013@daiict.ac.in>
2022-01-25 19:34:00 +0530

    CheckPoint15

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import { useState } from "react";
 import "./App.css";
+import React, { useState } from "react";
 import About from "./Components/About";
+import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = "#2f5a82";
-    }
-    else{
-      setMode('light');
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#4f6571";
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
-  }
+  };
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} toggleMode={toggleMode} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+                toggleMode={toggleMode}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -5,49 +5,55 @@ export default function TextForm(props) {
     // console.log("Uppercase was clicked" + text);
     let newtext = text.toUpperCase();
     setText(newtext);
+    props.showAlert("Converted to UpperCase","success");
   };
-
+  
   const handleDownClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newtext = text.toLowerCase();
     setText(newtext);
+    props.showAlert("Converted to LowerCase","success");
   };
-
+  
   const handleClearClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newtext = "";
     setText(newtext);
+    props.showAlert("Cleared Text","success");
   };
-
+  
   const handleTitleClick = () => {
     // console.log("Uppercase was clicked" + text);
     function titleCase(str) {
       return str
-        .toLowerCase()
-        .split(" ")
-        .map(function (word) {
-          return word.charAt(0).toUpperCase() + word.slice(1);
-        })
-        .join(" ");
+      .toLowerCase()
+      .split(" ")
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
     }
-
+    
     let newtext = titleCase(text);
     setText(newtext);
+    props.showAlert("Converted to TitleCase","success");
   };
-
+  
   const handleCopy = () => {
     let newText = document.getElementById("myBox");
     newText.select();
     navigator.clipboard.writeText(newText.value);
+    props.showAlert("Copied to ClipBoard","success");
   };
-
+  
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert("Spaces are removed","success");
   };
 
   const handleOnChange = (event) => {
-    console.log("On Change");
+    // console.log("On Change");
     setText(event.target.value);
   };
 
@@ -58,7 +64,7 @@ export default function TextForm(props) {
       <div className="container" style={{color: props.mode==="dark"?"white":"#2f5a82"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange= {handleOnChange} style={{backgroundColor: props.mode==="dark"?"grey":"white", color: props.mode==="dark"?"white":"#2f5a82"}} id="myBox" rows="8"></textarea>
+          <textarea className="form-control" value={text} onChange= {handleOnChange} style={{backgroundColor: props.mode==="dark"?"#2f5a88":"white", color: props.mode==="dark"?"white":"#2f5a82"}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
           Convert to UpperCase
